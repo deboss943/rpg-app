af04d43d3d850c589d7d1fdf7a3f704f1b9d4a0c
Ekomjah Denis <ekomjahedet@gmail.com>
2026-01-02 23:21:37 +0100

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -7,4 +7,6 @@ def create_character(name,strength, intelligence, charisma):
         return "The character should have a name"
     if len(name) > 10:
         return "The character name is too long"
+    if ' ' in name: 
+        return "The character name should not contain spaces"
 
